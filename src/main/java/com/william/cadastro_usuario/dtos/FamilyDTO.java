e46e6332f8d72c51c41b8fb7454e558d289ba8fa
wkoju84 <wkoju84@gmail.com>
2026-03-21 18:51:04 -0300

    adicionando familyDTO em dtos

---
+++ src/main/java/com/william/cadastro_usuario/dtos/FamilyDTO.java
@@ -0,0 +1,16 @@
+package com.william.cadastro_usuario.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FamilyDTO {
+
+    private Long id;
+    private String description;
+}
